37f4cbe1ffc217d2437ade76b0c48394dfd7cebf
Administrateur <administrateur@MacBook-Pro-de-Administrateur.local>
2015-12-29 15:49:43 +0100

    First commit

---
+++ ChoseAFaire.swift
@@ -0,0 +1,13 @@
+//
+//  ChoseAFaire.swift
+//  TheGroceryList
+//
+//  Created by Administrateur on 28/12/2015.
+//  Copyright © 2015 www.com.epsi.projeti5.grocerylist. All rights reserved.
+//
+
+import UIKit
+
+class ChoseAFaire: NSObject {
+
+}
